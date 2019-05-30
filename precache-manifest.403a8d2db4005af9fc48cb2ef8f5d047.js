a3d08187b27e370e2217c3e3c93e71eed6f7c193
Itamar <doriitamar@gmail.com>
2019-05-30 20:34:55 +0300

    initial site

---
+++ precache-manifest.403a8d2db4005af9fc48cb2ef8f5d047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f404b68a6bcaa83d7289",
+    "url": "/js/chunk-vendors.3c5b0444.js"
+  },
+  {
+    "revision": "b4b7e1af2acd8dc42311",
+    "url": "/js/app.2b53d895.js"
+  },
+  {
+    "revision": "d6a3302fd4d912d0ec49f8c2c31df3c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f404b68a6bcaa83d7289",
+    "url": "/css/chunk-vendors.e84bd57e.css"
+  },
+  {
+    "revision": "b4b7e1af2acd8dc42311",
+    "url": "/css/app.c181b1ba.css"
+  }
+];
